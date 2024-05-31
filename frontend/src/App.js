51e509babd4f4d066ea032efe93989c9acd35ce9
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-05-31 15:22:54 +0700

    update branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Banner from './components/Banner';
+import About from './components/About';
 
 function App(props) {
   const [banners, setBanners] = useState([]);
@@ -25,6 +26,10 @@ function App(props) {
         return <Banner title={banner.title} description={banner.description} image={banner.image} />
       })}
       {/* <Banner title={<>Guicci <br />Kit</>} description={"The href attribute requires a valid value to be accessible. Provide a valid, navigable address as the href value. If you cannot provide a valid href, but still need the element to resemble a link, use a button and change it with appropriate styles."}/> */}
+
+      {/* about section start */}
+      <About />
+      {/* about section end */}
     </div>
   );
 }
